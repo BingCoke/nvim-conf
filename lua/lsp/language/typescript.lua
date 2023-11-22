ee9f24c9442a8e1950e465d290d59c3d1e453fcc
bingcoke <248677315@qq.com>
2023-11-22 11:58:56 +0800

    daily

--- lua/lsp/language/typescript.lua
+++ lua/lsp/language/typescript.lua
@@ -1,5 +1,6 @@
 -- local typescript_setup, typescript = pcall(require, "typescript")
 local M = {}
+local util = require("lspconfig.util")
 
 function M.setup(lsp, default_capabilities, on_attach)
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -16,31 +17,38 @@ function M.setup(lsp, default_capabilities, on_attach)
 		},
 	}
 
-	local make_capabilities = require("typescript-tools.capabilities")
 	require("typescript-tools").setup({
 		on_attach = function(cli, buf)
 			on_attach(cli, buf)
 		end,
+		root_dir = function(fname)
+			-- INFO: stealed from:
+			-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/tsserver.lua#L22
+			local root_dir = util.root_pattern(".git")(fname)
+
+			-- INFO: this is needed to make sure we don't pick up root_dir inside node_modules
+			--[[local node_modules_index = root_dir and root_dir:find("node_modules", 1, true)
+			if node_modules_index and node_modules_index > 0 then
+				root_dir = root_dir:sub(1, node_modules_index - 2)
+			end]]
+
+			return root_dir
+		end,
+
 		--handlers = { ... },
 		capabilities = capabilities,
 		settings = {
-			-- spawn additional tsserver instance to calculate diagnostics on it
 			separate_diagnostic_server = true,
-			-- "change"|"insert_leave" determine when the client asks the server about diagnostic
-			publish_diagnostic_on = "insert_leave",
-			-- array of strings("fix_all"|"add_missing_imports"|"remove_unused")
-			-- specify commands exposed as code_actions
 			expose_as_code_action = {},
+			preferences = {
+				includeCompletionsForModuleExports = true,
+			},
 			tsserver_plugins = {
-				-- for TypeScript v4.9+
-				--"@styled/typescript-styled-plugin",
-				-- or for older TypeScript versions
-				--"typescript-styled-plugin",
 				"typescript-plugin-css-modules",
 			},
 			tsserver_file_preferences = {
 				includeInlayParameterNameHints = "all",
-				includeCompletionsForModuleExports = true,
+				includeCompletionsForModuleExports = false,
 				quotePreference = "single",
 			},
 		},
--- lua/plugin-config/neotree.lua
+++ lua/plugin-config/neotree.lua
@@ -165,7 +165,6 @@ require("neo-tree").setup({
 				--".null-ls_*",
 			},
 		},
-		follow_current_file = false, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = false, -- when true, empty folders will be grouped together
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -202,7 +201,6 @@ require("neo-tree").setup({
 		commands = {}, -- Add a custom command or override a global one using the same function name
 	},
 	buffers = {
-		follow_current_file = true, -- This will find and focus the file in the active buffer every
 		-- time the current file is changed while the tree is open.
 		group_empty_dirs = true, -- when true, empty folders will be grouped together
 		show_unloaded = true,
